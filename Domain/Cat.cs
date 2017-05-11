8515114d45cd36e822ee996fa40ce589f6bdd72f
InSyn <Synergy95@yandex.ru>
2017-05-11 17:50:39 +0700

    Add files via upload

---
+++ Domain/Cat.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Cat
+    {
+        public enum CatColor
+        {
+            Зелёный,
+            Жёлтый,
+            Красный
+        }
+
+        private int _age;
+
+        private CatColor _color;
+
+        private int _health = 8;
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(_name))
+                    _name = value;
+                else
+                    Console.WriteLine("Вы уже дали имя питомцу :(");
+            }
+        }
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                try
+                {
+                    while (value < 1 || value > 20)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Некорректный возвраст, попробуйте ещё раз...\n");
+                        value = int.Parse(Console.ReadLine());
+                    }
+                    _age = value;
+                }
+                catch
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        public CatColor Color
+        {
+            get
+            {
+                if (_health > 1 && _health <= 3)
+                    _color = CatColor.Красный;
+                if (_health > 3 && _health < 7)
+                    _color = CatColor.Жёлтый;
+                if (_health >= 7 && _health <= 10)
+                    _color = CatColor.Зелёный;
+                return _color;
+            }
+        }
+
+        public void Feed()
+        {
+            if (_health <= 10)
+            {
+                _health += 1;
+                Console.WriteLine("Котейка поел и доволен\n" +
+                                  "Здоровье увеличено на 1");
+            }
+            else
+            {
+                Console.WriteLine("Котейка сыт и здоров, незачем его кормить");
+            }
+        }
+
+        public void Punish()
+        {
+            if (_health > 1)
+            {
+                Console.WriteLine("Аккуратнее, котейка шипит и кусается\n" +
+                                  "Здоровье уменьшено на 1");
+                _health -= 1;
+            }
+            else
+            {
+                Console.WriteLine("Незачем его убивать :(");
+            }
+        }
+    }
+}
---
+++ ZooShop/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading;
+using Domain;
+
+namespace ZooShop
+{
+    public class ZooMenu
+    {
+        public Cat.CatColor PCatColor;
+        public string PurchasedCatName;
+        public string PurchsedCatAge;
+
+        public void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                "1.Показать информацию о питомце.\n" +
+                "2.Дать котейке имя.\n" +
+                "|------------------------|\n" +
+                "3.Покормить котейку.\n" +
+                "4.Наказать котейку.\n" +
+                "5.Узнать состояние котейки.\n" +
+                "|------------------------|\n" +
+                "0.Выйти.\n\n\n" +
+                "Информация о вашем питомце\n" +
+                "Имя: " + PurchasedCatName +
+                "\nВозраст: " + PurchsedCatAge +
+                "\nЦвет питомца: " + PCatColor);
+        }
+
+        public string StartPage()
+        {
+            Console.WriteLine("Спасибо за приобретение котейки програмиста\n" +
+                              "\n" +
+                              "\n" +
+                              "Укажите возраст вашего питомца: ");
+            PurchsedCatAge = Console.ReadLine();
+            return PurchsedCatAge;
+        }
+
+        public void InfoShow()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                "Информация о вашем питомце\n" +
+                "Имя: " + PurchasedCatName +
+                "\nВозраст: " + PurchsedCatAge +
+                "\nЦвет питомца: " + PCatColor +
+                "\n\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var ZMenu = new ZooMenu();
+            var PurchasedCat = new Cat();
+
+            PurchasedCat.Age = int.Parse(ZMenu.StartPage());
+
+            string a;
+            do
+            {
+                ZMenu.DisplayMenu();
+                Console.WriteLine("\nДля выбора пункта меню нажмите соответствующую клавишу...");
+                a = Console.ReadLine();
+
+                switch (a)
+                {
+                    case "1":
+                        ZMenu.InfoShow();
+                        break;
+                    case "2":
+                        Console.WriteLine(
+                            "Вы хотите дать своему питомцу имя!\n\t Помните, его нельзя будет изменить: ");
+                        PurchasedCat.Name = Console.ReadLine();
+                        ZMenu.PurchasedCatName = PurchasedCat.Name;
+                        Thread.Sleep(2800);
+                        break;
+                    case "3":
+                        PurchasedCat.Feed();
+                        ZMenu.PCatColor = PurchasedCat.Color;
+                        Thread.Sleep(1200);
+                        break;
+                    case "4":
+                        PurchasedCat.Punish();
+                        ZMenu.PCatColor = PurchasedCat.Color;
+                        Thread.Sleep(1200);
+                        break;
+                    case "5":
+                        Console.WriteLine("Котейка выглядит {0}", ZMenu.PCatColor);
+                        Thread.Sleep(1200);
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.Clear();
+                        ZMenu.DisplayMenu();
+                        break;
+                }
+            } while (a != "0");
+        }
+    }
+}
